31470fca6a82c0e1ddbae059545b19548975116b
Rolando Velazquez <velazquezchirolando@gmail.com>
2019-04-24 23:59:55 -0500

    Stateless

--- 1_creacion_basica_de_apps/stateless_app/lib/main.dart
+++ 1_creacion_basica_de_apps/stateless_app/lib/main.dart
@@ -12,35 +12,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text("Hola", style: TextStyle(color: Colors.black)),
-        actions: <Widget>[
-          IconButton(
-            icon: new Image.asset('images/IconUser.png'),
-            iconSize: 10.0,
-            onPressed: () {
-              print("object");
-            },
-          )
-        ],
-      ),
-      body: Body()
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text("Hola", style: TextStyle(color: Colors.black)),
+          actions: <Widget>[
+            IconButton(
+              icon: new Image.asset('images/IconUser.png'),
+              iconSize: 10.0,
+              onPressed: () {
+                print("object");
+              },
+            )
+          ],
+        ),
+        body: Body());
   }
 }
 
-class Body extends StatelessWidget{
+class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(child: RaisedButton(
-      child: Text("Hola"),
-      onPressed: (){
-        print("object");
-      },
-    ),);
+    return Center(
+      child: RaisedButton(
+        child: Text("Hola"),
+        onPressed: () {
+          print("object");
+        },
+      ),
+    );
   }
-
 }
